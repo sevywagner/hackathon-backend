1bc0e30b9f75ccfb492d006eab4d0bbcdb3c12e0
sevywagner <73050647+sevywagner@users.noreply.github.com>
2023-04-02 04:33:23 -0500

    more fields on order model

--- controllers/orders.js
+++ controllers/orders.js
@@ -3,10 +3,13 @@ const Order = require('./../models/order');
 exports.postOrder = (req, res, next) => {
     const name = req.body.name;
     const address = req.body.address;
+    const email = req.body.email;
+    const date = req.body.date;
+    const time = req.body.time;
     const bloodAmount = req.body.amount;
     const payout = req.body.payout;
 
-    const order = new Order(name, address, bloodAmount, payout);
+    const order = new Order(name, address, email, date, time, bloodAmount, payout);
     order.save().then(() => {
         res.status(201).json({
             message: 'Successfully placed order'
